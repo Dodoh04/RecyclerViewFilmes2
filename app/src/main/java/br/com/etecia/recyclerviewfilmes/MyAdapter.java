ae7861de6da509825ecd6433b4b6b94b6685656b
Dodoh04 <douglasoliversa3@gmail.com>
2023-03-21 21:03:43 -0300

    Correndo atras do professor

---
+++ app/src/main/java/br/com/etecia/recyclerviewfilmes/MyAdapter.java
@@ -0,0 +1,72 @@
+package br.com.etecia.recyclerviewfilmes;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private Context mContexto;
+    private List<Filmes> lstFilmes;
+
+    //criando o construtor da classe com parâmetros
+
+
+    public MyAdapter(Context mContexto, List<Filmes> lstFilmes) {
+        this.mContexto = mContexto;
+        this.lstFilmes = lstFilmes;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view;
+
+        LayoutInflater inflater = LayoutInflater.from(mContexto);
+        view = inflater.inflate(R.layout.modelo_filmes,parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        holder.idTituloFilmes.
+                setText(lstFilmes.get(position)
+                        .getTitulo());
+        holder.idImagemFilmes.
+                setImageResource(
+                        lstFilmes.get(position)
+                                .getImagem());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return lstFilmes.size();
+    }
+
+    //criar a classe ViewHolder
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView idTituloFilmes;
+        ImageView idImagemFilmes;
+        CardView idCardFilmes;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            idTituloFilmes = itemView.findViewById(R.id.idTituloFilmes);
+            idImagemFilmes = itemView.findViewById(R.id.idImgFilmes);
+            idCardFilmes = itemView.findViewById(R.id.idCardFilmes);
+        }
+    }
+}
