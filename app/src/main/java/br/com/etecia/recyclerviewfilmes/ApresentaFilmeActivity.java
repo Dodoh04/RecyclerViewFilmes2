1f4d5d06d5abafbfed90bb6b2c9e9540ac8f9f63
Dodoh04 <douglasoliversa3@gmail.com>
2023-03-21 22:09:51 -0300

    vamo embora

--- app/src/main/java/br/com/etecia/recyclerviewfilmes/ApresentaFilmeActivity.java
+++ app/src/main/java/br/com/etecia/recyclerviewfilmes/ApresentaFilmeActivity.java
@@ -2,7 +2,12 @@ package br.com.etecia.recyclerviewfilmes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ApresentaFilmeActivity extends AppCompatActivity {
 
@@ -10,5 +15,41 @@ public class ApresentaFilmeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.apresenta_filme_layout);
+
+        TextView mTitulo, mCategoria, mDescricao;
+
+        ImageView mImagem;
+
+        Button btnVoltar;
+
+        mTitulo = findViewById(R.id.mTituloFilme);
+        mCategoria = findViewById(R.id.mCategoria);
+        mDescricao = findViewById(R.id.mDescricao);
+        mImagem = findViewById(R.id.mImagemFilme);
+        btnVoltar = findViewById(R.id.btnVoltar);
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+        Intent intent = getIntent();
+
+        String titulo, descricao, categoria;
+        int imagemFilme;
+
+        titulo = intent.getExtras().getString("Titulo");
+        descricao = intent.getExtras().getString("Descricao");
+        categoria = intent.getExtras().getString("Categoria");
+
+        imagemFilme = intent.getExtras().getInt("imagemFime");
+
+        mTitulo.setText(titulo);
+        mDescricao.setText(descricao);
+        mCategoria.setText(categoria);
+        mImagem.setImageResource(imagemFilme);
+
     }
 }
--- app/src/main/java/br/com/etecia/recyclerviewfilmes/MyAdapter.java
+++ app/src/main/java/br/com/etecia/recyclerviewfilmes/MyAdapter.java
@@ -1,6 +1,7 @@
 package br.com.etecia.recyclerviewfilmes;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,7 +34,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         View view;
 
         LayoutInflater inflater = LayoutInflater.from(mContexto);
-        view = inflater.inflate(R.layout.modelo_filmes,parent,false);
+        view = inflater.inflate(R.layout.modelo_filmes, parent, false);
 
         return new ViewHolder(view);
     }
@@ -48,6 +49,19 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
                 setImageResource(
                         lstFilmes.get(position)
                                 .getImagem());
+        holder.idCardFilmes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContexto, ApresentaFilmeActivity.class);
+
+                intent.putExtra("Titulo", lstFilmes.get(position).getTitulo());
+                intent.putExtra("Descricao", lstFilmes.get(position).getDescricao());
+                intent.putExtra("Categoria", lstFilmes.get(position).getCategoria());
+                intent.putExtra("ImagemFilme", lstFilmes.get(position).getImagem());
+
+                mContexto.startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+            }
+        });
 
     }
 
@@ -57,7 +71,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     }
 
     //criar a classe ViewHolder
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
         TextView idTituloFilmes;
         ImageView idImagemFilmes;
         CardView idCardFilmes;
